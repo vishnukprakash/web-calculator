5ee79941a151699286fa9427fd1d2574a7a27163
Vishnu Prakash <vishnu.prakash@6dtech.co.in>
2020-03-17 10:31:53 +0530

    logger-added

--- src/main/java/org/vishnu/webcalculator/webservice/CalculatorRestEndPoint.java
+++ src/main/java/org/vishnu/webcalculator/webservice/CalculatorRestEndPoint.java
@@ -8,15 +8,22 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.vishnu.webcalculator.core.CalculatorService;
 import org.vishnu.webcalculator.core.CalculatorServiceFactoryDefImpl;
 
 @Path("/")
 @Singleton
 public class CalculatorRestEndPoint {
+	
+	private static final Logger LOGGER = LogManager.getLogger(CalculatorRestEndPoint.class);
+	
 	private final CalculatorService calculatorService;
 	
 	public CalculatorRestEndPoint() {
+		LOGGER.info("CalculatorRestEndPoint() invoked");
+		
 		this.calculatorService = new CalculatorServiceFactoryDefImpl()
 										.createCalculatorService();
 	}
@@ -26,6 +33,9 @@ public class CalculatorRestEndPoint {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response add(Request request) {
+		
+		LOGGER.info("add() invoked: [{}, {}]", request.getA(), request.getB());
+		
 		long result = calculatorService.add(request.getA(), request.getB());
 		return Response.ok(new ResponseData(result)).build();
 	}
@@ -35,6 +45,9 @@ public class CalculatorRestEndPoint {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response substract(Request request) {
+		
+		LOGGER.info("substract() invoked: [{}, {}]", request.getA(), request.getB());
+		
 		long result = calculatorService.substract(request.getA(), request.getB());
 		return Response.ok(new ResponseData(result)).build();
 	}
@@ -44,6 +57,9 @@ public class CalculatorRestEndPoint {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response multiply(Request request) {
+		
+		LOGGER.info("multiply() invoked: [{}, {}]", request.getA(), request.getB());
+		
 		long result = calculatorService.multiply(request.getA(), request.getB());
 		return Response.ok(new ResponseData(result)).build();
 	}
@@ -53,6 +69,9 @@ public class CalculatorRestEndPoint {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response divide(Request request) {
+		
+		LOGGER.info("divide() invoked: [{}, {}]", request.getA(), request.getB());
+		
 		long result = calculatorService.divide(request.getA(), request.getB());
 		return Response.ok(new ResponseData(result)).build();
 	}
